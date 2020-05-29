26e2d88a076eb02e58ab96a178a8a209af7a3e76
Yatin Singla <yatinsingla2@gmail.com>
2020-05-28 17:17:45 -0700

    ?Due for Refactor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React from "react";
-import logo from "./database.svg";
+import React, { Component } from "react";
+// import logo from "./database.svg";
 import "./App.css";
 import Template from "./components/Template";
 import BusinessSearch from "./components/BusinessSearch";
 
-function App() {
-  return (
-    <div className="App">
-      <Template />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
-      </header> */}
-      <BusinessSearch />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:4000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Template />
+        <BusinessSearch />
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
